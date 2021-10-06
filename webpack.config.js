67af95a308ccf1959ce9baadce6714b45f580d00
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-06 16:29:00 +0800

    将JS转义为低版本

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//webpack.config.js
+module.exports = {
+  module: {
+      rules: [
+          {
+              test: /\.jsx?$/, //匹配规则
+              use: ['babel-loader'],
+              exclude: /node_modules/ //排除 node_modules 目录
+          }
+      ]
+  }
+}
